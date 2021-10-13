942f88d2a4c86eebc895b70534624db7de2785d5
kimnewbie <gracecancho@gmail.com>
2021-10-13 23:25:56 +0900

    tree test

--- src/componts/AntTreeView.js
+++ src/componts/AntTreeView.js
@@ -40,7 +40,7 @@ const AntTreeView = () => {
         selectedKeys: [],
     });
 
-    const onExpand = (expandedKeys) => {
+    const onExpandCheckable = (expandedKeys) => {
         console.log('onExpand', expandedKeys);
         // if not set autoExpandParent to false, if children expanded, parent can not collapse.
         // or, you can remove all expanded children keys.
@@ -49,11 +49,11 @@ const AntTreeView = () => {
             autoExpandParent: false,
         });
     }
-    const onCheck = (checkedKeys) => {
+    const onCheckCheckable = (checkedKeys) => {
         console.log('onCheck', checkedKeys);
         setState({ checkedKeys });
     }
-    const onSelect = (selectedKeys, info) => {
+    const onSelectCheckable = (selectedKeys, info) => {
         console.log('onSelect', info);
         setState({ selectedKeys });
     }
@@ -70,20 +70,55 @@ const AntTreeView = () => {
         });
     }
 
+    const onSelectShowLine = (selectedKeys, info) => {
+        console.log('selected', selectedKeys, info);
+    }
+
     return (
         <div>
-            <Tree
-                checkable
-                onExpand={onExpand}
-                expandedKeys={state.expandedKeys}
-                autoExpandParent={state.autoExpandParent}
-                onCheck={onCheck}
-                checkedKeys={state.checkedKeys}
-                onSelect={onSelect}
-                selectedKeys={state.selectedKeys}
-            >
-                {renderTreeNodes(treeData)}
-            </Tree>
+            <div>
+
+                <Tree
+                    checkable
+                    onExpand={onExpandCheckable}
+                    expandedKeys={state.expandedKeys}
+                    autoExpandParent={state.autoExpandParent}
+                    onCheck={onCheckCheckable}
+                    checkedKeys={state.checkedKeys}
+                    onSelect={onSelectCheckable}
+                    selectedKeys={state.selectedKeys}
+                >
+                    {renderTreeNodes(treeData)}
+                </Tree>
+            </div>
+            <hr />
+            <div>
+                <Tree
+                    showLine
+                    defaultExpandedKeys={['0-2']}
+                    onSelect={onSelectShowLine}
+                >
+                    <TreeNode title="parent 2" key="0-2">
+                        <TreeNode title="parent 2-1" key="2-0-1" />
+                        <TreeNode title="parent 2-2" key="2-0-2" />
+                        <TreeNode title="parent 2-3" key="2-0-3" />
+                    </TreeNode>
+                    <TreeNode title="parent 1" key="0-1">
+                        <TreeNode title="parent 1-0" key="0-0-0">
+                            <TreeNode title="leaf" key="0-0-0-0" />
+                            <TreeNode title="leaf" key="0-0-0-1" />
+                            <TreeNode title="leaf" key="0-0-0-2" />
+                        </TreeNode>
+                        <TreeNode title="parent 1-1" key="0-0-1">
+                            <TreeNode title="leaf" key="0-0-1-0" />
+                        </TreeNode>
+                        <TreeNode title="parent 1-2" key="0-0-2">
+                            <TreeNode title="leaf" key="0-0-2-0" />
+                            <TreeNode title="leaf" key="0-0-2-1" />
+                        </TreeNode>
+                    </TreeNode>
+                </Tree>
+            </div>
         </div >
     )
 }
